956f4d1fe695b7026c2c8aa8dedac54359a619c7
BoomshakalakaNL <boomshakalaka.nl@gmail.com>
2018-06-25 19:22:36 +0200

    Nieuwe functionaliteit
    Je mag geen declaratie bewerken die al is gevalideerd.

--- pages/declaraties/edit.js
+++ pages/declaraties/edit.js
@@ -25,6 +25,7 @@ class EditDeclaration extends Component {
     client: this.props.summary[1],
     dateDeclaration: this.props.summary[6],
     dateEndDeclaration: this.props.summary[7],
+    isValidated: this.props.summary[8],
     errorMessage: '',
     loading: false,
     declarations: getDeclarationFromSummary(this.props.summary[3], this.props.summary[4], this.props.summary[5])
@@ -96,65 +97,76 @@ class EditDeclaration extends Component {
   }
 
   render(){
-    return (
-      <Layout>
-        <h4>Bewerken Declaration</h4>
-        <Form onSubmit={this.handleSubmit} loading={this.state.loading} error={!!this.state.errorMessage}>
-          <Form.Field>
-            <label>Address van Verzekeraar:</label>
-            <Input
-              value={ this.state.insurance}
-              onChange={ event => this.setState( {insurance: event.target.value} ) }
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>Address van Cliënt:</label>
-            <Input
-              value={ this.state.client}
-              onChange={ event => this.setState( {client: event.target.value} ) }
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>Declaratie datum:</label>
-            <Input
-              value={ this.state.dateDeclaration}
-              type="date"
-              onChange={ event => this.setState( {dateDeclaration: event.target.value} ) }
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>Declaratie eind datum:</label>
-            <Input
-              value={ this.state.dateEndDeclaration}
-              type="date"
-              onChange={ event => this.setState( {dateEndDeclaration: event.target.value} ) }
-            />
-          </Form.Field>
+    if (!this.state.isValidated) {
+      return (
+        <Layout>
+          <h4>Bewerken Declaration</h4>
+          <Form onSubmit={this.handleSubmit} loading={this.state.loading} error={!!this.state.errorMessage}>
+            <Message error header="Oops!" content={this.state.errorMessage} />
+            <Form.Field>
+              <label>Address van Verzekeraar:</label>
+              <Input
+                value={ this.state.insurance}
+                onChange={ event => this.setState( {insurance: event.target.value} ) }
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Address van Cliënt:</label>
+              <Input
+                value={ this.state.client}
+                onChange={ event => this.setState( {client: event.target.value} ) }
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Declaratie datum:</label>
+              <Input
+                value={ this.state.dateDeclaration}
+                type="date"
+                onChange={ event => this.setState( {dateDeclaration: event.target.value} ) }
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Declaratie eind datum:</label>
+              <Input
+                value={ this.state.dateEndDeclaration}
+                type="date"
+                onChange={ event => this.setState( {dateEndDeclaration: event.target.value} ) }
+              />
+            </Form.Field>
 
 
-          {this.state.declarations.map((declaration, idx) => (
-            <Form.Group widths='equal'>
-              <Form.Field>
-                <label>Code</label>
-                <Input fluid value={declaration.code} onChange={this.handleDeclrationCodeChange(idx)}/>
-              </Form.Field>
-              <Form.Field>
-                <label>Aantal</label>
-                <Input fluid type='number' value={declaration.amount} onChange={this.handleDeclarationAmountChange(idx)}/>
-              </Form.Field>
-              <Form.Field>
-                <label>Prijs</label>
-                <Input fluid type='number' step='0.01' value={declaration.price} onChange={this.handleDeclarationPriceChange(idx)}/>
-              </Form.Field>
-              <Button type="button" style={{height: '38px', marginTop: '23px'}} onClick={this.handleRemoveDeclaration(idx)} warning>Remove</Button>
-            </Form.Group>
-          ))}
-          <Button type="button" onClick={this.handleAddDeclaration} className="small" success>Add Declaration</Button>
-          <br/>
-          <Button type="submit" style={{marginTop: '40px'}} primary>Verstuur</Button>
-        </Form>
-      </Layout>
-    );
+            {this.state.declarations.map((declaration, idx) => (
+              <Form.Group widths='equal'>
+                <Form.Field>
+                  <label>Code</label>
+                  <Input fluid value={declaration.code} onChange={this.handleDeclrationCodeChange(idx)}/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Aantal</label>
+                  <Input fluid type='number' value={declaration.amount} onChange={this.handleDeclarationAmountChange(idx)}/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Prijs</label>
+                  <Input fluid type='number' step='0.01' value={declaration.price} onChange={this.handleDeclarationPriceChange(idx)}/>
+                </Form.Field>
+                <Button type="button" style={{height: '38px', marginTop: '23px'}} onClick={this.handleRemoveDeclaration(idx)} warning>Remove</Button>
+              </Form.Group>
+            ))}
+            <Button type="button" onClick={this.handleAddDeclaration} className="small" success>Add Declaration</Button>
+            <br/>
+            <Button type="submit" style={{marginTop: '40px'}} primary>Verstuur</Button>
+          </Form>
+        </Layout>
+      );
+    }
+    else {
+      return(
+        <Layout>
+          <h4>U probeert een declaratie te bewerken die intern is gevalideerd. Dit mag niet, foei.</h4>
+        </Layout>
+      );
+    }
+
   }
 }
 export default EditDeclaration;
